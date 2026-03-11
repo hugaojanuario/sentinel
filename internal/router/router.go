beec443e010bcc460c0931733f86367db42bc487
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-03-11 02:57:41 -0300

    feat: status this memory, network ..., implemented

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,18 +1,12 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/hugaojanuario/sentinel/internal/controllers"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
-
-	router.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "OK"})
-	})
 
 	router.GET("/containers", controllers.ListContainers)
 
